1acd497950c14bb9b6939020a1319e06cd844b26
Sean Keane <seankeane93@gmail.com>
2022-07-15 12:37:21 -0700

    Created logic/form to input orders

---
+++ VendorOrder/Controllers/OrderControllers.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using BakeyStore.Models;
+
+namespace BakeryStore.Controllers
+{
+  public class OrderController : Controller
+  {
+    [HttpGet("/vendors/{id}/orders/new")]
+    public ActionResult New(int id)
+    {
+      Vendor newVendor = Vendor.Find(id);
+      return View(newVendor);
+    }
+
+    [HttpGet("vendors/{id}/orders/{orderId}")]
+    public ActionResult Show(int id, int orderId)
+    {
+      Order selectedOrder = Order.Find(orderId);
+      Vendor selectedVendor = Vendor.Find(id);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("orders", selectedOrder);
+      model.Add("vendors", selectedVendor);
+      return View(model);
+    }
+  }
+}
