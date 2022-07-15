91c77766df8c439a8be03961c728c42d73e9b236
Sean Keane <seankeane93@gmail.com>
2022-07-15 16:52:36 -0700

    WIP thoroughly testing Vendor.cs

--- VendorOrder.Tests/ModelTests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/ModelTests/VendorTests.cs
@@ -6,5 +6,19 @@ using System;
 namespace VendorOrder.Tests
 
 {
-  
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearVendors();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_InstantiateVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Luke Skywalker", "Moisture farmer on Tatooine"); 
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
