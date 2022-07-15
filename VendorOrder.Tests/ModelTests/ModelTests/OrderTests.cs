54f2cfc44fd74978d9c8d89e6bdc836cb64d3e5e
Sean Keane <seankeane93@gmail.com>
2022-07-15 15:19:01 -0700

    WIP thoroughly testing Models

---
+++ VendorOrder.Tests/ModelTests/ModelTests/OrderTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+
+{
+   [TestClass]
+   public class ItemTests : IDisposable
+   {
+    public void Dispose()
+    {
+      Order.ClearOrders();
+    }
+}
+}
---
+++ VendorOrder.Tests/ModelTests/ModelTests/VendorTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+
+{
+  
+}
