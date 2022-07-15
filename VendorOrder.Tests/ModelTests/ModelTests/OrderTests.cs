b3de6d5da77849dacde62698c6dacd0411cd4b0a
Sean Keane <seankeane93@gmail.com>
2022-07-15 15:56:17 -0700

    WIP testing Models/Order.cs.  All tests are currently passing.

--- VendorOrder.Tests/ModelTests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/ModelTests/OrderTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System;
 
 namespace VendorOrder.Tests
-
 {
    [TestClass]
    public class ItemTests : IDisposable
@@ -19,6 +18,21 @@ public void OrderConstructor_BuildsOrder_Order()
 {
   Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
   Assert.AreEqual(typeof(Order), newOrder.GetType());
+}
+
+[TestMethod]
+public void GetName_ReturnName_String()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Assert.AreEqual("Daffy Duck",newOrder.Name);
+}
+
+[TestMethod]
+public void GetDescription_ReturnDescription_String()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Assert.AreEqual("15 pastries",newOrder.Description);
+
 }
 }
 }
