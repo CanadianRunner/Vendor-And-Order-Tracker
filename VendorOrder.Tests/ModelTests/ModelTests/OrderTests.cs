c59013eef209e9f9af51f3a48ce134b856bab991
Sean Keane <seankeane93@gmail.com>
2022-07-15 16:13:58 -0700

    Thoroughly tested all Models for Order.cs.  Logic is sound and passing.

--- VendorOrder.Tests/ModelTests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/ModelTests/OrderTests.cs
@@ -32,7 +32,45 @@ public void GetDescription_ReturnDescription_String()
 {
   Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
   Assert.AreEqual("15 pastries",newOrder.Description);
+}
 
+[TestMethod]
+public void GetPrice_ReturnPrice_Int()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Assert.AreEqual(83,newOrder.Price);
+}
+
+[TestMethod]
+public void GetDate_ReturnDate_String()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Assert.AreEqual("2022-07-15",newOrder.Date);
+}
+
+[TestMethod]
+public void GetId_ReturnId_Int()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Assert.AreEqual(1,newOrder.Id);
+}
+
+[TestMethod]
+public void GetAllOrders_ReturnsAllOrders_OrderList()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Order secondOrder = new Order("Cthulhu", "150 pastries", 400, "2023-04-10");
+  List<Order> allOrders = new List<Order> {newOrder, secondOrder};
+  CollectionAssert.AreEqual(allOrders, Order.GetAllOrders());
+}
+
+[TestMethod]
+public void Find_ReturnsCorrectOrderIndex_OrderId()
+{
+  Order newOrder = new Order("Daffy Duck", "15 pastries", 83, "2022-07-15");
+  Order secondOrder = new Order("Cthulhu", "150 pastries", 400, "2023-04-10");
+  List<Order> allOrders = new List<Order> {newOrder, secondOrder};
+  Assert.AreEqual(secondOrder, Order.Find(2));
 }
 }
 }
