5682d8772532cdee667c3125ffba784df8450da2
Sean Keane <seankeane93@gmail.com>
2022-07-15 17:06:39 -0700

    Developed passing tests utilizing RGR.  Logic is passing for Instantiating Vendor,  Name, and Description.

--- VendorOrder.Tests/ModelTests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/ModelTests/VendorTests.cs
@@ -20,5 +20,19 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("Luke Skywalker", "Moisture farmer on Tatooine"); 
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnName_String()
+    {
+      Vendor newVendor = new Vendor("Luke Skywalker", "Moisture farmer on Tatooine"); 
+      Assert.AreEqual("Luke Skywalker", newVendor.Name);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      Vendor newVendor = new Vendor("Luke Skywalker", "Moisture farmer on Tatooine"); 
+      Assert.AreEqual("Moisture farmer on Tatooine", newVendor.Description);
+    }
   }
 }
